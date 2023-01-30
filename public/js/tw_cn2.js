bb31243285c62c2cc3229aed81abb70bf426564f
谷啟祥 <evan.gu@piapp.com.tw>
2023-01-30 14:59:42 +0800

    docker modify & delete js file

--- public/js/tw_cn2.js
+++
@@ -1,8 +0,0 @@
-
-var defaultEncoding = 1;
-var translateDelay = 0;
-var cookieDomain = "http://www.tw-5859.com.tw/";
-var msgToTraditionalChinese = "蝜�擃�";
-var msgToSimplifiedChinese = "蝪⊿��";
-var translateButtonId = "translateLink";
-translateInitilization();
